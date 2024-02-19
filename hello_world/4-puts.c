7f236e71c94e9e60612ea98f6645c6fb3585935a
Ch3ncho <ernestoasenciotorres@hotmail.com>
2024-02-19 19:08:49 -0400

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-#include <stdio> int main() { a="Programming is like building a multilingual puzzle" puts(a); return 0; }
+gcc  { a="Programming is like building a multilingual puzzle" puts(a); return 0; }
